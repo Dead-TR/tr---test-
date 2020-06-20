e043f88c9b6c52238e8f3bba9de49e1c8b6e13bc
DTR <t.terry.rice@gmail.com>
2020-06-20 18:42:47 +0300

    Updates

---
+++ precache-manifest.a7f45c4415a7d14232a1e376e444a94d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9a7690a2ed3cb688a806e991fd6fd7",
+    "url": "/tr---test/index.html"
+  },
+  {
+    "revision": "aaf3d5a1176932792763",
+    "url": "/tr---test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2b03f65bdedbc7eef32",
+    "url": "/tr---test/static/js/2.af6dc0e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tr---test/static/js/2.af6dc0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf3d5a1176932792763",
+    "url": "/tr---test/static/js/main.5af2ffbb.chunk.js"
+  },
+  {
+    "revision": "5149fde09885a625f7ff",
+    "url": "/tr---test/static/js/runtime-main.2bd186ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tr---test/static/media/logo.5d5d9eef.svg"
+  }
+]);
